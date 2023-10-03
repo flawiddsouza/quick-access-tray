fa7a5d7b2a6b62d79d963f1b282fdda1b6ea952a
Flawid DSouza <me@flawiddsouza.com>
2023-10-03 11:14:14 +0530

    fix: command window pops up for a second when running a command on windows (compiled with ldflags -H=windowsgui)

--- helpers.go
+++ helpers.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"syscall"
 
 	"gopkg.in/yaml.v3"
 )
@@ -61,6 +62,9 @@ func runCommand(command string) {
 	println("Running command:", command)
 	split_command := splitString(command)
 	cmd := exec.Command(split_command[0], split_command[1:]...)
+	if runtime.GOOS == "windows" {
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true} // so that a cmd window doesn't pop up
+	}
 	err := cmd.Run()
 	if err != nil {
 		println("Failed to run command:", err)
